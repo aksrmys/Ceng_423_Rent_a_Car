d74abef684c9704d87889ddf5af7021332653b8e
EdanurSARIKAYA <100278578+EdanurSARIKAYA@users.noreply.github.com>
2023-06-17 17:49:41 +0300

    Add files via upload

--- Ceng_423_Rent_a_Car/Ceng_423_Rent_a_Car/Controllers/RentController.cs
+++ Ceng_423_Rent_a_Car/Ceng_423_Rent_a_Car/Controllers/RentController.cs
@@ -3,17 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Ceng_423_Rent_a_Car.Models;
+using Ceng_423_Rent_a_Car.Repository;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Ceng_423_Rent_a_Car.Controllers
 {
     public class RentController : Controller
     {
+        private readonly IData data;
+        public RentController(IData data)
+        {
+            this.data = data;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Rent rent)
+        {
+            if (!ModelState.IsValid)
+                return View(rent);
+            ViewBag.isSaved = data.BookingNow(rent);
+            ModelState.Clear();
+            return View();
+        }
+        [HttpGet]
+
+        public IActionResult GetBrand() {
+            var list = data.GetBrand();
+            return Json(list);
+        }
+        [HttpGet]
+
+        public IActionResult GetModel(string brand)
+        {
+            var list = data.GetModel(brand);
+            return Json(list);
+        }
+        [HttpGet]
+
+        public IActionResult GetDriver() 
+        {
+            var list = data.GetAllDrivers();
+            return Json(list);
         }
     }
 }
