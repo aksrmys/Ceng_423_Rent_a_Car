1f90d9c86c702a968b3037e7a054d0136374592f
Rümeysa Akış <80596533+aksrmys@users.noreply.github.com>
2023-06-17 15:26:36 +0300

    Add files via upload

--- Ceng_423_Rent_a_Car/Ceng_423_Rent_a_Car/Program.cs
+++ Ceng_423_Rent_a_Car/Ceng_423_Rent_a_Car/Program.cs
@@ -1,16 +1,18 @@
+﻿using Ceng_423_Rent_a_Car.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient(typeof(IData), typeof(Data));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -21,7 +23,8 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
