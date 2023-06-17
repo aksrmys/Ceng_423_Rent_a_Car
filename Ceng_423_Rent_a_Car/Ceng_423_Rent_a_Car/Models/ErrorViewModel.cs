936e6580519e927f0c9609d34c507e214bc8d359
MelihUluhan <108068455+MelihUluhan@users.noreply.github.com>
2023-06-17 15:07:00 +0300

    Add files via upload

---
+++ Ceng_423_Rent_a_Car/Ceng_423_Rent_a_Car/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ceng_423_Rent_a_Car.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
